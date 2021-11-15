71b97984ef1547567a2060495659883bff730b8e
fiha18 <fihaas786@gmail.com>
2021-11-15 21:17:09 +0530

    Input format Created with some changes

--- maxMovies.cpp
+++ maxMovies.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-vector<string> simple_tokenizer(string s)
-{
-	vector<string> words;
-    stringstream ss(s);
-    string word;
-    while (ss >> word) {
-        words.push_back(word);
-    }
-    return words;
-}
-
-
 int dateToInt(string month, int day)
 {
 	unordered_map<string,int> map;
